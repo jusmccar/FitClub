3ab8e863d5040535f0030e835baa16646b492889
jusmccar <justinmccartney1@yahoo.com>
2022-07-01 16:18:43 -0700

    Add navigation bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,14 @@ const Header = () => {
     return (
         <div className="header">
             <img src={Logo} alt="" className="logo" />
+
+            <ul className="header-menu">
+                <li>Home</li>
+                <li>Programs</li>
+                <li>About Us</li>
+                <li>Plans</li>
+                <li>Testimonials</li>
+            </ul>
         </div>
     );
 };
