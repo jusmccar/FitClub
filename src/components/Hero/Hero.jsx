7ab503d78e8ea399a023529d5f517e10563a459c
jusmccar <justinmccartney1@yahoo.com>
2022-07-01 16:07:31 -0700

    Add Header image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "../Header/Header";
 import "./Hero.css";
 
 const Hero = () => {
     return(
         <div className="hero">
             <div className="hero-left">
-
+                <Header/>
             </div>
             <div className="hero-right">
 
