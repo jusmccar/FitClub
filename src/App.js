31f342cf3f09711df3173d395ce7e7e6f3408c5b
jusmccar <justinmccartney1@yahoo.com>
2022-07-01 15:57:42 -0700

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Hero from "./components/Hero/Hero";
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}></span>
+          <span style={{color: 'white'}}>
+            <Hero/>
+          </span>
     </div>
   );
 }
