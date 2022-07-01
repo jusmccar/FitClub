7521ed1de47376c21b13995c05d63db3e6d6ed89
jusmccar <justinmccartney1@yahoo.com>
2022-07-01 15:58:32 -0700

    Split Hero component into left and right

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,12 @@ import "./Hero.css";
 const Hero = () => {
     return(
         <div className="hero">
+            <div className="hero-left">
 
+            </div>
+            <div className="hero-right">
+
+            </div>
         </div>
     );
 };
