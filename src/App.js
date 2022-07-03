fa77ac8899283d6b5cd90df8a9dbd8d107879c07
jusmccar <justinmccartney1@yahoo.com>
2022-07-03 16:43:05 -0700

    Add plans component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
+import Plans from "./components/Plans/Plans";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
             <Hero/>
             <Programs/>
             <Reasons/>
+            <Plans/>
           </span>
     </div>
   );
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {plansData} from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+import "./Plans.css";
+
+const Plans = () => {
+    return (
+        <div className="plans">
+            <div className="programs-header" id="plans-header">
+                <span className="stroke-text">READY TO START</span>
+                <span>YOUR JOURNEY</span>
+                <span className="stroke-text">NOW WITH US</span>
+            </div>
+            <div className="plans-card">
+                {plansData.map((plan, i) => (
+                    <div className="plan-card">
+                        <div className="plan-icon">{plan.icon}</div>
+                        <span className="plan-name">{plan.name}</span>
+                        <span className="plan-price">${plan.price}</span>
+                        <div className="features">
+                            {plan.features.map((feature, i) => (
+                                <div className="feature">
+                                    <img src={whiteTick} alt="" />
+                                    <span key={i}>{feature}</span>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="see-more">
+                            <div>
+                                <span>See more benefits -></span>
+                            </div>
+                            <button className="btn">Join now</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Plans;
